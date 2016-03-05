d9309fd406b456f2cfa4bf90949e7fcca8c82247
daniel chung <dclovesdc@sbcglobal.net>
2016-03-04 17:58:16 -0800

    increase retry to 10 for cc1111

--- libpump/pcall.c
+++ libpump/pcall.c
@@ -45,7 +45,7 @@ _pcall(Pcall *tx, Pcall *rx)
 		preamblems = 2000;
 		timeoutms = 200;
 #ifdef	CC1111
-		tries = 7;
+		tries = 10;
 #else
 		tries = 5;
 #endif
@@ -60,7 +60,7 @@ _pcall(Pcall *tx, Pcall *rx)
 		preamblems = 0;
 		timeoutms = 500;
 #ifdef	CC1111
-		tries = 7;
+		tries = 10;
 #else
 		tries = 3;
 #endif
@@ -76,7 +76,7 @@ _pcall(Pcall *tx, Pcall *rx)
 		taskdelay(rx->backoffms);
 		tx1.type = Tkeepalive;
 #ifdef	CC1111
-		if((rv=_pcall1(&tx1, rx, timeoutms*2, 0, 7)) != 1)
+		if((rv=_pcall1(&tx1, rx, timeoutms*2, 0, 10)) != 1)
 #else
 		if((rv=_pcall1(&tx1, rx, timeoutms*2, 0, 2)) != 1)
 #endif
